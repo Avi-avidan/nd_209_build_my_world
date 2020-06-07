f318c737c91c3ef62034a3e1096084c3121484da
Avi <avidan2012@gmail.com>
2020-06-07 12:24:36 +0000

    myhome

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# nd_209_build_my_world
-# nd_209_build_my_world
+#### nd_209_build_my_world
---
+++ script/welcome_message.cpp
@@ -0,0 +1,18 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Hello,\nWelcome to BIDI's World!\n");
+              printf("It's great to see ya :)\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
